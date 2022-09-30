78fe5781f4a3023e70949776db90128a95c1a1af
Alain <alaindelonm@gmail.com>
2022-09-30 08:46:58 -0300

    fix: Exercicios lista 2

--- modulo1/Lista2/src/Questao3.java
+++ modulo1/Lista2/src/Questao3.java
@@ -7,13 +7,17 @@ public class Questao3 {
         String nome = null;
         String maisVelho = null;
         String maisPesado = null;
+        final String VALOR_DE_SAIDA = "SAIR";
         int idade, quantidadeCadastrados = 0, maiorIdade = 0;
         double peso, altura,somaAlturas = 0, mediaAlturas = 0, alturaMaiorJogador = 0, maiorPeso = 0;
 
 
-        while(nome != "SAIR"){
+        while(nome != VALOR_DE_SAIDA){
             System.out.println("Digite o nome do jogador: ");
             nome = input.nextLine();
+            if(nome == VALOR_DE_SAIDA){
+                break;
+            }
             System.out.println("Digite a altura do jogador:");
             altura = input.nextDouble();
             System.out.println("Digite a idade do jogador: ");
@@ -44,7 +48,7 @@ public class Questao3 {
         System.out.println("Altura do maior jogador: " + alturaMaiorJogador);
         System.out.println("Jogador mais velho: " + maisVelho);
         System.out.println("Jogador mais pesado: " + maisPesado);
-        System.out.printf("Média das alturas dos jogadores: %.2f", mediaAlturas);
+        System.out.printf("Média das alturas dos jogadores: %.2f\n", mediaAlturas);
         System.out.println("=======================================================");
         input.close();
 
